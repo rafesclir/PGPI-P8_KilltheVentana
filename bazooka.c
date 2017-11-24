554a2c3e42abbda4c36fcde444c877813e1b1cca
angelsantiago94 <angellsan94@gmail.com>
2017-11-24 13:36:06 +0100

    Creadando el bazooka
    Solución de #7

---
+++ bazooka.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+int carga;
+printf ("Inserte la carga: ");
+scanf ("%d",&i);
+printf ("Cargando\n");
+sleep(3);
+printf ("Cargado\n");
+printf ("Disparano...\n");
+sleep(3);
+printf ("El bazooka esta tan confuso que se disparó a si mismo\n");
+return 0;
+}
