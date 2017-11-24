ef229d863f9b7090fde77e16ace73fac7e486412
angelsantiago94 <angellsan94@gmail.com>
2017-11-24 13:56:05 +0100

    Añadiendo control remoto a la bomba

--- bomba.c
+++ bomba.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 int main(){
 int i;
+char c;
+printf("Pulse una tecla para iniciar la bomba \n");
+scanf ("%c",c);  
 for(i=10;i>=0;i--){
 printf("%d \n",i);
+sleep(1);
 }
 printf("BOOOMMM!!!");
 return 0;
